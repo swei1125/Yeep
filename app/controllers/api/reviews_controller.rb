18ba477e6a6df1d7b1f8010b9812f0368b6d05b9
Natasha <wsyalways2@gmail.com>
2018-08-14 23:06:07 -0700

    Create methods of create, update, and delete in reviews controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,2 +1,33 @@
 class Api::ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.biz_id = params[:biz_id]
+    @biz = @review.biz
+    if @review.save
+      render 'api/bizs/show'
+    else
+      render json: @review.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @biz = @review.biz
+    if @review.update
+      render 'api/bizs/show'
+    else
+      render json: @review.errors.full_messages, status: 422
+    end
+  end
+
+  def delete
+    review = Review.find(params[:id])
+    review.destroy
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:body, :rating)
+  end
 end
