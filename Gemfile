84ff891365c55f3e94d618946f5bca9059b416a1
Natasha <wsyalways2@gmail.com>
2018-08-17 11:50:20 -0700

    Complete the seed data and fixed the map component fisrt mount issue

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'capistrano-rails', group: :development
 
 gem "aws-sdk-s3"
-gem "figaro"
+gem "figaro", '~> 1.1.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- frontend/components/map/bizs_map.jsx
+++ frontend/components/map/bizs_map.jsx
@@ -16,28 +16,31 @@ class BizsMap extends React.Component {
     });
   }
 
-  componentDidMount() {
-    const location = this.props.match.params.location;
-    const center = location === 'default' ? 'San Francisco' : location;
-    let geocoder = new google.maps.Geocoder();
-    const mapOptions = {
-      center: { lat: 0, lng: 0 },
-      zoom: 12
-    };
-    this.map = new google.maps.Map(this.mapNode, mapOptions);
-    const that = this;
-    geocoder.geocode({'address': center}, function(results, status) {
-      if (status === 'OK') {
-        that.map.setCenter(results[0].geometry.location);
-        that.props.bizs.forEach(biz => that.createMarker(biz));
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
+  // componentDidMount() {
+  //   const location = this.props.match.params.location;
+  //   const center = location === 'default' ? 'San Francisco' : location;
+  //   let geocoder = new google.maps.Geocoder();
+  //   const mapOptions = {
+  //     center: { lat: 0, lng: 0 },
+  //     zoom: 12
+  //   };
+  //   this.map = new google.maps.Map(this.mapNode, mapOptions);
+  //   const that = this;
+  //   geocoder.geocode({'address': center}, function(results, status) {
+  //     if (status === 'OK') {
+  //       that.map.setCenter(results[0].geometry.location);
+  //       that.props.bizs.forEach(biz => that.createMarker(biz));
+  //     } else {
+  //       alert('Geocode was not successful for the following reason: ' + status);
+  //     }
+  //   });
+  // }
 
   componentWillReceiveProps(newProps) {
-    if (this.props.match.url !== newProps.match.url) {
+    const urlDiff = this.props.match.url !== newProps.match.url;
+    const bizsDiff = this.props.bizs.length !== newProps.bizs.length;
+    console.log(newProps);
+    if (urlDiff || bizsDiff) {
       const location = newProps.match.params.location;
       const center = location === 'default' ? 'San Francisco' : location;
       let geocoder = new google.maps.Geocoder();
@@ -50,7 +53,7 @@ class BizsMap extends React.Component {
       geocoder.geocode({'address': center}, function(results, status) {
         if (status === 'OK') {
           that.map.setCenter(results[0].geometry.location);
-          that.props.bizs.forEach(biz => that.createMarker(biz));
+          newProps.bizs.forEach(biz => that.createMarker(biz));
         } else {
           alert('Geocode was not successful for the following reason: ' + status);
         }
